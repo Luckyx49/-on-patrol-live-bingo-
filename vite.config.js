0f1a5193cac7a7b61821d453fd23bf2748aba1bd
Luckyx49 <xxluckystfuxx@gmail.com>
2025-08-10 18:48:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Set base to '/<your-repo-name>/' for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/on-patrol-live-bingo-plus/',
+  base: '-on-patrol-live-bingo',
 })
